d0eddedf7ae02f0a1b23d51a32d1101745569cbc
edufo <edufo@DESKTOP-NIH4RAC>
2023-12-22 09:32:40 +0100

    Mensaje

---
+++ src/main/java/org/cuatrovientos/dam/ed/ejemplo2ev/EdEvaluacion2/App.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.dam.ed.ejemplo2ev.EdEvaluacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
